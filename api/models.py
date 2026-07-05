2f7e11dd0a49d94fd431ba11e385f91824c5351a
lconf-ubuntu <lyflconf@163.com>
2026-07-05 16:25:44 +0800

    fix:修复4个数据校验Bug并更新测试报告

--- api/models.py
+++ api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
 
 
 class Category(models.Model):
@@ -16,8 +17,8 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=200, verbose_name='商品名称')
     description = models.TextField(blank=True, verbose_name='商品描述')
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='价格')
-    stock = models.IntegerField(default=0, verbose_name='库存')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='价格', validators=[MinValueValidator(0)])
+    stock = models.IntegerField(default=0, validators=[MinValueValidator(0)], verbose_name='库存')
     category = models.ForeignKey(
         Category, on_delete=models.CASCADE, related_name='products',
         verbose_name='分类'
@@ -34,7 +35,7 @@ class Cart(models.Model):
         User, on_delete=models.CASCADE, related_name='cart_items'
     )
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.IntegerField(default=1, verbose_name='数量')
+    quantity = models.IntegerField(default=1, validators=[MinValueValidator(1)], verbose_name='数量')
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
